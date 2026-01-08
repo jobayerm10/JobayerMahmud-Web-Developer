1e67dc703cb0e31e4ae34663dac25553b69ae2de
Jobayer Mahmud <jobayermahmud976@gmail.com>
2026-01-08 19:41:04 +0600

    updated

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 const ProjectCard = (props) => {
   return (
     <>
-      <div className="w-1/2 h-full group transition-all  relative border-5 border-gray-500 rounded-2xl hover:rounded-[60px] overflow-hidden ">
+      <div className="w-1/2 h-full group transition-all  relative rounded-2xl hover:rounded-[60px] overflow-hidden ">
         <img className="h-full w-full object-cover" src={props.image1} alt="" />
         <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex justify-center items-center top-0 left-0 h-full w-full bg-black/30 ">
           <h2 className="text-5xl font-[font1] uppercase border-4 px-3 py-2 rounded-full cursor-pointer text-white border-white">
@@ -9,7 +9,7 @@ const ProjectCard = (props) => {
           </h2>
         </div>
       </div>
-      <div className="w-1/2 h-full group transition-all  relative border-5 border-gray-500 rounded-2xl hover:rounded-[60px] overflow-hidden ">
+      <div className="w-1/2 h-full group transition-all  relative rounded-2xl hover:rounded-[60px] overflow-hidden ">
         <img className="h-full w-full object-cover" src={props.image2} alt="" />
         <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex justify-center items-center top-0 left-0 h-full w-full bg-black/30 ">
           <h2 className="text-5xl font-[font1] uppercase border-4 px-3 py-2 rounded-full cursor-pointer text-white border-white">
