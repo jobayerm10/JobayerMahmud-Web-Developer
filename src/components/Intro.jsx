6895fc04d18f511c8fcb6de5fe13a0f41438e80c
Jobayer Mahmud <jobayermahmud976@gmail.com>
2026-01-09 16:41:33 +0600

    services section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,13 @@ const Intro = () => {
       <h3 className="lg:text-[1.5vw] -mb-5 md:text-[10vw] text-(--text-color) ">
         👋, My name is Jobayer and I am an aspiring
       </h3>
-      <img className="relative z-10" src={image} alt="" />
+      <img
+        className="relative z-10   w-[80vw] md:w-[65vw] lg:w-[24vw]
+    h-auto
+    max-w-full"
+        src={image}
+        alt=""
+      />
 
       <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
         <div className="relative">
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div>
-      <h1>this is all my projects</h1>
+    <div className=" ">
+      <div className=" mt-[10vw] mx-auto max-w-[70vw]">
+        <div className="uppercase flex items-center justify-center gap-[2vw]  ">
+          <h5 className="text-[#FDA228] tracking-[0.4em]">
+            Frontend Web Developer
+          </h5>
+          <h5 className="text-[#FDA228] tracking-[0.4em]">& UI Enthusiast</h5>
+        </div>
+        <p className="text-center text-[4.2vw] font-[font1]">
+          Impactful branding and high-end <br /> productive websites for
+          creatives.
+        </p>
+        <div className="-tracking-[0.1vw] flex items-center justify-center">
+          <a
+            target="_blank"
+            href=""
+            className="  text-[.9vw] px-5 py-3  border-2 rounded-md bg-(--bg-color) text-(--text-color)  transition-all duration-300 ease-out hover:px-10 shadow-md hover:shadow-lg
+     "
+          >
+            Let's start a project together
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
