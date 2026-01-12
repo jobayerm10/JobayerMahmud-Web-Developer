a405690d656bba874d9c6f9376d1d759805d987c
Jobayer Mahmud <jobayermahmud976@gmail.com>
2026-01-12 17:10:50 +0600

    responsive

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -45,7 +45,7 @@ const Intro = () => {
           <a
             target="_blank"
             href="https://cdn.jsdelivr.net/gh/jobayerm10/Jobayer-Mahmud-Resume/Jobayer-Mahmud-Resume.pdf"
-            className="text-sm md:text-base px-6 py-3 border rounded-md
+            className="text-center text-sm md:text-base px-6 py-3 border rounded-md
       bg-(--bg-color) text-(--text-color)
       transition-all duration-300 hover:px-10"
           >
